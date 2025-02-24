f8eeb8039431117136f74f1f67b988524320a62a
CannyRo <whatcannydev@gmail.com>
2025-02-24 17:32:45 +0100

    formated code

--- src/question.js
+++ src/question.js
@@ -1,18 +1,18 @@
 class Question {
-    // YOUR CODE HERE
-    constructor(text, choices, answer, difficulty){
-        this.text = text
-        this.choices = choices
-        this.answer = answer
-        this.difficulty = difficulty
+  // YOUR CODE HERE
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  shuffleChoices() {
+    const shuffleArray = [];
+    const temporaryArray = this.choices;
+    for (let i = this.choices.length - 1; i >= 0; i--) {
+      const randomIndex = Math.floor(Math.random() * i);
+      shuffleArray.push(temporaryArray[randomIndex]);
+      temporaryArray.splice(randomIndex, 1);
     }
-    shuffleChoices(){
-        const shuffleArray = []
-        const temporaryArray = this.choices
-        for (let i = this.choices.length-1; i>=0; i--){
-            const randomIndex = Math.floor(Math.random() * (i))
-            shuffleArray.push(temporaryArray[randomIndex])
-            temporaryArray.splice(randomIndex,1)
-        }
-}
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,42 +1,37 @@
 class Quiz {
-    // YOUR CODE HERE:
-    
-    constructor (questions, timeLimit, timeRemaining){
-    this.questions = questions
-    this.timeLimit = timeLimit
-    this.timeRemaining = timeRemaining
-    this.correctAnswers = 0
-    this.currentQuestionIndex = 0
+  // YOUR CODE HERE
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  shuffleQuestions() {
+    const newShuffle = [];
+    const temporaryShuffle = this.questions;
+    for (let i = this.questions.length - 1; i >= 0; i--) {
+      const randomIndex = Math.floor(Math.random() * i);
+      newShuffle.push(temporaryShuffle[randomIndex]);
+      temporaryShuffle.splice(randomIndex, 1);
     }
-    getQuestion(){
-        return this.questions[this.currentQuestionIndex]
+  }
+  checkAnswer(answer) {
+    console.log(this.questions[this.currentQuestionIndex]);
+    if (this.questions[this.currentQuestionIndex].answer === answer) {
+      this.correctAnswers++;
     }
-    
-    moveToNextQuestion(){
-        this.currentQuestionIndex++;
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
     }
-
-    shuffleQuestions(){
-        const newShuffle= []
-        const temporaryShuffle = this.questions
-        for (let i = this.questions.length-1; i>=0; i--){
-            const randomIndex = Math.floor(Math.random() * (i))
-            newShuffle.push(temporaryShuffle[randomIndex])
-            temporaryShuffle.splice(randomIndex,1)
-        }
-    }
-
-    checkAnswer(answer){
-        console.log(this.questions[this.currentQuestionIndex])
-        if (this.questions[this.currentQuestionIndex].answer=== answer){
-             this.correctAnswers++;
-        }
-    }
-
-    hasEnded(){
-        if (this.currentQuestionIndex < this.questions.length){
-        return false;
-        }
-        return true;
-    }
+    return true;
+  }
 }
